4438234aabc6dd92a0b8f206d493aef2f47f98ce
SK IMTIAZ <skimtiaz86@gmail.com>
2024-02-14 13:34:12 +0530

    update

--- application/views/gallery.php
+++ application/views/gallery.php
@@ -109,7 +109,7 @@
 
                         <div class="col-md-4 grid-item mb-4 px-4" data-groups='["medicine"]'>
                             <div class="portfolio-item rounded">
-                                <img src="https://via.placeholder.com/300" alt="Image 2">
+                                <img src="https://images.unsplash.com/photo-1707823585193-850a928684e7?q=80&w=1528&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D/300" alt="Image 2">
                                 <div class="portfolio-overlay">
                                     <a class="portfolio-description portfolio-img" href="#previewModal2" data-bs-toggle="modal" data-bs-target="#previewModal2">
                                         <div class="portfolio-info">
